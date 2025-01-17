5c83c780a2ee64fda8104ee661235160f3c177cb
RafaBonach <rafa.bonach@gmail.com>
2025-01-16 21:04:42 -0300

    Quase terminando o projeto 2

--- Projeto2/projeto2.py
+++ Projeto2/projeto2.py
@@ -1,153 +1,204 @@
-import networkx as nx
+import matplotlib.pyplot as plt # Biblioteca responsável por criar as imagens do grafo
+from matplotlib import pylab as pl # Biblioteca responsável por criar as imagens do grafo
+import imageio # Biblioteca responsavel por fazer o gif do grafo.
+import networkx as nx # Biblioteca responsavel por manipular o grafo
+from networkx.drawing.layout import bipartite_layout
 import copy
 
-# Primeiro, precisamos ler os arquivos e armazena-los em um vetor
-listaProjetos = {}
-arq = open('ListaProjetos.txt', 'r')
-for linha in arq:
-    linha = linha.strip()
-    linha = linha.strip('()')
-    linha = linha.replace(',', '')
-    linha = linha.split(' ')
-    listaProjetos[linha[0]] = [int(linha[1]), int(linha[2]), []]
-
-    #print(linha)
-arq.close()
-
+# Para facilitar a manipulação, vamos deixar as listas como variaveis Globais.
 listaAlunos = {}
+listaProjetos = {}
 
-arq = open('ListaAlunos.txt', 'r')
-for linha in arq:
-    linha = linha.strip()
-    linha = linha.split(':')
-    indice = linha[0].strip('()')
+def le_entradas():
+    # Abre o arquivo para leitura
+    with open('entradaProj2.24TAG.txt', 'r') as arquivo:
+        # Lê linha a linha do arquivo
+        for linha in arquivo:
+            # Verifica se é a linha de leitura
+            if linha[0:1] == '(':
+                # Se for um Projeto, processa ele e armazena no dicionario listaProjetos
+                if linha[1:2] == 'P':
+                    # Remove espaços no inicio e fim da linha
+                    linha = linha.strip()
+                    # Remove () no inicio e fim da linha
+                    linha = linha.strip('()')
+                    # Troca , por ''
+                    linha = linha.replace(',', '')
+                    # Separa os valores
+                    linha = linha.split(' ')
+                    # Incrementa os valores no dicionario junto com uma lista de integrantes do projeto
+                    listaProjetos[linha[0]] = [int(linha[1]), int(linha[2]), []]
 
-    linha[1] = linha[1].split(') ')
-    interece = linha[1][0]
-    interece = interece.strip('()')
-    interece = interece.replace(',', '')
-    interece = interece.split(' ')
-    nota = linha[1][1]
-    nota = int(nota.strip('()'))
+                # Se for um aluno, processa ele e armazena no dicionario listaAlunos
+                elif linha[1:2] == 'A':
+                    # Remove espaços no inicio e fim da linha
+                    linha = linha.strip()
+                    # Separa as informações da linha
+                    nome, informacoes = linha.split(':')
+                    # Remove () no inicio e fim da linha
+                    nome = nome.strip('()')
 
-    listaAlunos[indice] = [interece, nota]
-    # print(indice, listaAlunos[indice])
-arq.close()
+                    informacoes = informacoes.strip('()')
+                    # Separa as informações pelo ) já que possuem informações que não são separadas por espaço, como é o caso do aluno 177
+                    interece, nota = informacoes.split(')')
+                    # Troca , por ''
+                    interece = interece.replace(',', '')
+                    # separa as informações
+                    interece = interece.split(' ')
 
-# Agora, vamos criar um digrafo apenas com vertices com base nos dois vetores criados
-"""
-grafo = nx.DiGraph()
-for noProjeto in listaProjetos:
-    grafo.add_node(noProjeto[0])
+                    # Remove espaços no inicio e fim da linha
+                    nota = nota.strip()
+                    # Remove () no inicio e fim da linha
+                    nota = nota.strip('()')
+                    # Transforma em int
+                    nota = int(nota)
 
-for noAluno in listaAlunos.keys():
-    grafo.add_node(noAluno)
+                    # Incrementa os valores no dicionario
+                    listaAlunos[nome] = [interece, nota]
 
-#print(grafo)
-"""
+def cria_grafo_bipartido():
+    grafo = nx.DiGraph()
+    projetos = [x for x in listaProjetos.keys()]
+    alunos = [x for x in listaAlunos.keys()]
+    grafo.add_nodes_from(projetos, bipartite=0)
+    grafo.add_nodes_from(alunos, bipartite=2)
 
-# Vamos agora fazer o emparelhamento estável
+    #pos = bipartite_layout(grafo, projetos) # Define a posição para caso queira desenhar na tela
+    return grafo
 
-listaCandidatos = []
-repescagem = []
-candidatosRepescagem = {}
-listaEmparelhamentos = {}
-copiaListaAlunos = copy.deepcopy(listaAlunos);
+def emparelhamento(grafo):
+    copiaListaAlunos = copy.deepcopy(listaAlunos);
+    listaCandidatos = [i for i in copiaListaAlunos.keys()]
+    listaEmparelhamentos = {i:j for i, j in copiaListaAlunos.items()}
 
-for i in copiaListaAlunos.keys():
-    listaCandidatos.append(i)
-for i in copiaListaAlunos.keys():
-    listaEmparelhamentos[i] = []
-#print("Aluno  Projeto")
-while len(listaCandidatos) > 0:
-    aluno = listaCandidatos[0]
 
-    while aluno in listaCandidatos and len(copiaListaAlunos[aluno][0]) > 0:
-        projeto = copiaListaAlunos[aluno][0].pop(0)
-        if copiaListaAlunos[aluno][1] >= listaProjetos[projeto][1]:
-            if len(listaProjetos[projeto][2]) >= listaProjetos[projeto][0]:
-                ultimoAluno = listaProjetos[projeto][2][-1]
-                if copiaListaAlunos[ultimoAluno][1] < copiaListaAlunos[aluno][1]:
-                    listaProjetos[projeto][2].remove(ultimoAluno)
-                    listaProjetos[projeto][2].append(aluno)
-                    listaCandidatos.remove(aluno)
-                    listaCandidatos.append(ultimoAluno)
 
-                    #listaEmparelhamentos[projeto].append(f'{projeto} X {ultimoAluno} -> {aluno}')
-                    """
-                    print(f'{aluno}: {listaAlunos[aluno][1]} - {projeto}: {listaProjetos[projeto][1]}')
-                    print(f'{aluno}: {listaAlunos[aluno][1]} VS {piorAluno}: {listaAlunos[piorAluno][1]}')
-                    print(listaProjetos[projeto][2], "\n")
-                    
-                else:
-                    print(f'{aluno}: {listaAlunos[aluno][1]} X {projeto}: {listaProjetos[projeto][1]}')
-                    print("COMPETIÇÃO")
-                    print(f'{aluno}: {listaAlunos[aluno][1]} VS {piorAluno}: {listaAlunos[piorAluno][1]}')
-                    print(listaProjetos[projeto][2], "\n")
-                    """
+    def algoritmo_Gale_Shapley():
+        repescagem = []
 
-            else:
-                listaProjetos[projeto][2].append(aluno)
-                listaCandidatos.remove(aluno)
-                #listaEmparelhamentos[projeto].append(f'{projeto} -> {aluno}')
-                """
-                print(f'{aluno}: {listaAlunos[aluno][1]} - {projeto}: {listaProjetos[projeto][1]}')
-                print(listaProjetos[projeto][2], "\n")
-                """
+        while len(listaCandidatos) > 0:
+            aluno = listaCandidatos[0]
 
-        else:
-            """print(f'{aluno}: {listaAlunos[aluno][1]} X {projeto}: {listaProjetos[projeto][1]}')
-            """
-            #print(listaProjetos[projeto][2], "\n")
+            while aluno in listaCandidatos and len(copiaListaAlunos[aluno][0]) > 0:
 
-    if len(copiaListaAlunos[aluno][0]) == 0 and aluno in listaCandidatos:
-        listaCandidatos.remove(aluno)
-        repescagem.append(aluno)
-
-listaIntermediaria = []
-for i in range(8):
-    repescagem.sort()
-    for j in repescagem:
-        candidatosRepescagem[j] = copy.deepcopy(listaAlunos[j]);
-
-    while len(repescagem) >0:
-        aluno = repescagem.pop(0)
-
-        while len(candidatosRepescagem[aluno][0]) > 0:
-            projeto = candidatosRepescagem[aluno][0].pop(0)
-
-            if candidatosRepescagem[aluno][1] >= listaProjetos[projeto][1]:
-                if len(listaProjetos[projeto][2]) >= listaProjetos[projeto][0]:
-                    ultimoAluno = listaProjetos[projeto][2][-1]
-                    if listaAlunos[aluno][1] > listaAlunos[ultimoAluno][1]:
-                        listaProjetos[projeto][2].remove(ultimoAluno)
-                        listaProjetos[projeto][2].append(aluno)
-                        aluno = ultimoAluno
-                        candidatosRepescagem[aluno] = copiaListaAlunos[aluno]
-
-                    elif listaAlunos[aluno][1] == listaAlunos[ultimoAluno][1]:
-                        if len(candidatosRepescagem[aluno][0]) >= len(copiaListaAlunos[ultimoAluno][0]):
+                projeto = copiaListaAlunos[aluno][0].pop(0)
+                if copiaListaAlunos[aluno][1] >= listaProjetos[projeto][1]:
+                    if len(listaProjetos[projeto][2]) >= listaProjetos[projeto][0]:
+                        ultimoAluno = listaProjetos[projeto][2][-1]
+                        if copiaListaAlunos[ultimoAluno][1] < copiaListaAlunos[aluno][1]:
                             listaProjetos[projeto][2].remove(ultimoAluno)
                             listaProjetos[projeto][2].append(aluno)
-                            aluno = ultimoAluno
-                            candidatosRepescagem[aluno] = copiaListaAlunos[aluno]
+                            listaCandidatos.remove(aluno)
+                            listaCandidatos.append(ultimoAluno)
+
+                            #cria aresta entre projeto e aluno
+                            grafo.add_edge(projeto, aluno)
+                            #remove aresta entre projeto e último aluno
+                            grafo.remove_edge(projeto, ultimoAluno)
+
+                            #listaEmparelhamentos[projeto].append(f'{projeto} X {ultimoAluno} -> {aluno}')
+                            """
+                            print(f'{aluno}: {listaAlunos[aluno][1]} - {projeto}: {listaProjetos[projeto][1]}')
+                            print(f'{aluno}: {listaAlunos[aluno][1]} VS {piorAluno}: {listaAlunos[piorAluno][1]}')
+                            print(listaProjetos[projeto][2], "\n")
+                            
+                        else:
+                            print(f'{aluno}: {listaAlunos[aluno][1]} X {projeto}: {listaProjetos[projeto][1]}')
+                            print("COMPETIÇÃO")
+                            print(f'{aluno}: {listaAlunos[aluno][1]} VS {piorAluno}: {listaAlunos[piorAluno][1]}')
+                            print(listaProjetos[projeto][2], "\n")
+                            """
+
+                    else:
+                        listaProjetos[projeto][2].append(aluno)
+                        listaCandidatos.remove(aluno)
+
+                        # cria aresta entre projeto e aluno
+                        grafo.add_edge(projeto, aluno)
+
+
+                        #listaEmparelhamentos[projeto].append(f'{projeto} -> {aluno}')
+                        """
+                        print(f'{aluno}: {listaAlunos[aluno][1]} - {projeto}: {listaProjetos[projeto][1]}')
+                        print(listaProjetos[projeto][2], "\n")
+                        """
 
                 else:
-                    listaProjetos[projeto][2].append(aluno)
-                    break
+                    """print(f'{aluno}: {listaAlunos[aluno][1]} X {projeto}: {listaProjetos[projeto][1]}')
+                    """
+                    #print(listaProjetos[projeto][2], "\n")
 
-        if len(candidatosRepescagem[aluno][0]) == 0:
-            listaIntermediaria.append(aluno)
+            if len(copiaListaAlunos[aluno][0]) == 0 and aluno in listaCandidatos:
+                listaCandidatos.remove(aluno)
+                repescagem.append(aluno)
 
-    repescagem = copy.deepcopy(listaIntermediaria)
+        return repescagem.copy()
 
-j = 0
-for i in listaProjetos.keys():
-    if len(listaProjetos[i][2]) == 0:
-        j += 1
-        print(i, '\n')
-    """if len(listaProjetos[i][2]) < listaProjetos[i][0] :
-        j += 1
-        print(i, '\n')"""
+    def emparelhamentoRepescagem(repescagem, candidatosRepescagem):
+        listaIntermediaria = []
+        while len(repescagem) >0:
+            aluno = repescagem.pop(0)
 
-print(j)
+            while len(candidatosRepescagem[aluno][0]) > 0:
+                projeto = candidatosRepescagem[aluno][0].pop(0)
+
+                if candidatosRepescagem[aluno][1] >= listaProjetos[projeto][1]:
+                    if len(listaProjetos[projeto][2]) >= listaProjetos[projeto][0]:
+                        ultimoAluno = listaProjetos[projeto][2][-1]
+                        if listaAlunos[aluno][1] > listaAlunos[ultimoAluno][1]:
+                            listaProjetos[projeto][2].remove(ultimoAluno)
+                            listaProjetos[projeto][2].append(aluno)
+
+                            # remove aresta entre projeto e último aluno
+                            grafo.remove_edge(projeto, ultimoAluno)
+                            # cria aresta entre projeto e aluno
+                            grafo.add_edge(projeto, aluno)
+
+                            # O ultimo aluno da lista do projeto, passa a ser o aluno que tentará outro projeto
+                            aluno = ultimoAluno
+                            # A lista de repescagem recebe os projetos que o aluno ainda não tentou
+                            candidatosRepescagem[aluno] = copiaListaAlunos[aluno]
+
+                        elif listaAlunos[aluno][1] == listaAlunos[ultimoAluno][1]:
+                            if len(candidatosRepescagem[aluno][0]) >= len(copiaListaAlunos[ultimoAluno][0]):
+                                listaProjetos[projeto][2].remove(ultimoAluno)
+                                listaProjetos[projeto][2].append(aluno)
+
+                                # remove aresta entre projeto e último aluno
+                                grafo.remove_edge(projeto, ultimoAluno)
+                                # cria aresta entre projeto e aluno
+                                grafo.add_edge(projeto, aluno)
+
+                                # O ultimo aluno da lista do projeto, passa a ser o aluno que tentará outro projeto
+                                aluno = ultimoAluno
+                                # A lista de repescagem recebe os projetos que o aluno ainda não tentou
+                                candidatosRepescagem[aluno] = copiaListaAlunos[aluno]
+
+                    else:
+                        listaProjetos[projeto][2].append(aluno)
+
+                        # cria aresta entre projeto e aluno
+                        grafo.add_edge(projeto, aluno)
+
+                        break
+
+            if len(candidatosRepescagem[aluno][0]) == 0:
+                listaIntermediaria.append(aluno)
+
+        return listaIntermediaria.copy()
+
+    repescagem = algoritmo_Gale_Shapley()
+
+    for i in range(9):
+        candidatosRepescagem = {i: copy.deepcopy(listaAlunos[i]) for i in repescagem}
+        repescagem = emparelhamentoRepescagem(repescagem, copy.deepcopy(candidatosRepescagem))
+
+    return grafo
+
+# Primeiro, precisamos realizar uma leitura do arquivo e manipular os dados antes de utiliza-los
+le_entradas()
+# Então, com os dados organizados, vamos criar um grafo bipartido para poder mostrar como ficou o emparelhamento
+grafo = cria_grafo_bipartido()
+# Então, realizamos o emparelhamento do grafo.
+grafo = emparelhamento(grafo)
+
